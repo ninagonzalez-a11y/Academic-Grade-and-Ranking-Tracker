d105dc6547953849885385375173a1ac99b1db37
Gilian <salofrancheskagilianb@gmail.com>
2026-06-05 12:45:36 +0800

    Fields and Methods

--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,7 @@ Responsibility: Single student data model
 Single reason to change: Student's stored fields change
 */
 
+// [UNDERSTAND]
 public class Student {
     private String name, idNumber, numericGrade ;
     private double rawGrade;
@@ -10,41 +11,46 @@ public class Student {
 
     public String getIdNumber(){
 
+        return "";
     }
 
-    public void setNumericGrade(String){
+    public void setNumericGrade(String numericGrade){
 
     }
 
     public double getRawGrade(){
 
+        return 0;
     }
 
-    public void setRawGrade(double){
+    public void setRawGrade(double rawGrade){
 
     }
 
-    public void setIdNumber(String){
+    public void setIdNumber(String idNumber){
 
     }
 
     public String getNumericGrade(){
 
+        return "";
     }
 
-    public void setLetterRank(char){
+    public void setLetterRank(char letterRank){
 
     }
 
     public String getName(){
 
+        return "";
     }
 
     public char getLetterRank(){
 
+        return 'c';
     }
 
-    public void setName(String){
+    public void setName(String name){
 
     }
 
