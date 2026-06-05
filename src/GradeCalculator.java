c378ee31759308d39688448216a9ce577956f0d8
panfia-prog <sofiasantostomas@gmail.com>
2026-06-05 13:12:06 +0800

    Created and updated GradeCalculator class with methods

---
+++ src/GradeCalculator.java
@@ -0,0 +1,30 @@
+public class GradeCalculator {
+    public double computeAverage(double[] grades){
+        double average = 0;
+
+        return average;
+    }
+    public String getRemarks(String remark){
+
+    }
+    public double computeRawGrade(double firstGrade, double secondGrade, double thirdGrade, double fourthGrade, double fifthGrade){
+        double rawGrade = 0;
+
+        return rawGrade;
+    }
+    public String assignNumericGrade(double rawGrade){
+        String numericGrade = " ";
+
+        return numericGrade;
+    }
+    public char assignLetterRank(double rawGrade){
+        char letterRank = ' ';
+
+        return letterRank;
+    }
+    public String getRemarks(char letterRank){
+        String remarks = " ";
+
+        return remarks;
+    }
+}
