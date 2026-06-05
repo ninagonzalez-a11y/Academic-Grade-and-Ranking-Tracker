5938e8617ce79d5d47585da49fc2505085119114
panfia-prog <sofiasantostomas@gmail.com>
2026-06-05 13:01:37 +0800

    Created and updated GradeConstants class with values

---
+++ src/GradeConstants.java
@@ -0,0 +1,18 @@
+public class GradeConstants {
+    static public int ID_LENGTH = 8;
+    static public int ID_DIVISOR = 11;
+    static public int FACULTY_THRESHOLD = 8;
+    static public int SEPARATOR = 15;
+    static public int MAX_STUDENTS = 10;
+    static public int NUM_MODULES = 5;
+
+    static public double MAX_SCORE = 100;
+    static public double MIN_SCORE = 0;
+
+    static public double TEACHER_WEIGHT = 0.05;
+    static public double PARTICIPATION_WEIGHT = 0.05;
+    static public double LAB_WEIGHT = 0.4;
+    static public double EXAM_WEIGHT = 0.2;
+    static public double PROJECT_WEIGHT = 0.3;
+
+}
