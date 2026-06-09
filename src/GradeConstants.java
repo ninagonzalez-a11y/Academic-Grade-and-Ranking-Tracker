10479300fef37768ed65d61a04203f042c5bb9f2
panfia-prog <sofiasantostomas@gmail.com>
2026-06-09 12:22:06 +0800

    Updated GradeConstant.java so it can be accessible via 'private GradeConstant C;' and added the computations for all the methods in GradeCalculator.java

--- src/GradeConstants.java
+++ src/GradeConstants.java
@@ -1,19 +1,19 @@
 public class GradeConstants {
-    static public int ID_LENGTH = 8;
-    static public int ID_DIVISOR = 11;
-    static public int FACULTY_THRESHOLD = 16; //uses the dot product >=16 from the prev code
+    public static final int ID_LENGTH = 8;
+    public static final int ID_DIVISOR = 11;
+    public static final int FACULTY_THRESHOLD = 16; //uses the dot product >=16 from the prev code
 
-    static public int SEPARATOR = 75; //line width for the separator
-    static public int MAX_STUDENTS = 20; //from the prev code
-    static public int NUM_MODULES = 5;
+    public static final int SEPARATOR = 75; //line width for the separator
+    public static final int MAX_STUDENTS = 20; //from the prev code
+    public static final int NUM_MODULES = 5;
 
-    static public double MAX_SCORE = 100;
-    static public double MIN_SCORE = 0;
+    public static final double MAX_SCORE = 100;
+    public static final double MIN_SCORE = 0;
 
-    static public double TEACHER_WEIGHT = 0.05;
-    static public double PARTICIPATION_WEIGHT = 0.05;
-    static public double LAB_WEIGHT = 0.4;
-    static public double EXAM_WEIGHT = 0.2;
-    static public double PROJECT_WEIGHT = 0.3;
+    public static final double TEACHER_WEIGHT = 0.05;
+    public static final double PARTICIPATION_WEIGHT = 0.05;
+    public static final double LAB_WEIGHT = 0.4;
+    public static final double EXAM_WEIGHT = 0.2;
+    public static final double PROJECT_WEIGHT = 0.3;
 
 }
