32c31d181824155dd7869f798a3982c0a083d0fd
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-09 08:12:26 +0800

    added the skeleton for ReportPrinter

---
+++ src/ReportPrinter.java
@@ -0,0 +1,16 @@
+public class ReportPrinter {
+    private C GradeConstants;
+    private calculator GradeCalculator;
+
+    void printClassStats(StudentRepository){
+
+    }
+
+    void printReport (StudentRepository){
+
+    }
+
+    void printSeparatorLine(){
+
+    }
+}
